90b88e1508bd04357a19df6da4f43a42dfcd9f5d
Kozjar <qwertzxc0000@gmail.com>
2019-06-05 20:11:14 +0300

    feat: add element adding functionality

--- src/components/FramesBar/FramesBar.js
+++ src/components/FramesBar/FramesBar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import FramePreview from './FramePreview';
 
 export default class FramesBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { frames: [], totalFramesCount: 0 };
+
+    this.addNewFrame = this.addNewFrame.bind(this);
+  }
+
+  addNewFrame() {
+    this.setState((state, props) => ({
+      frames: [...state.frames, { number: state.totalFramesCount, id: state.totalFramesCount }],
+      totalFramesCount: state.totalFramesCount + 1,
+    }));
+  }
+
   render() {
     return (
-      <div>
-
+      <div className="frames-bar">
+        {this.state.frames.map(frame => <FramePreview key={frame.id}
+                        number={frame.number}/>)}
+        <button onClick={this.addNewFrame}>
+          Add New Frame
+        </button>
       </div>
     );
   }
