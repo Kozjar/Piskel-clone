ae2dbc96a5ceb7806c969051ada70f6ea9c6587f
alexgrist14 <agurec.fasasf@gmail.com>
2019-06-06 17:36:30 +0300

    refactor: delete useless constructor

--- src/components/FramesBar/FramePreview.js
+++ src/components/FramesBar/FramePreview.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 export default class FramePreview extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="frames-bar__frame-preview">
