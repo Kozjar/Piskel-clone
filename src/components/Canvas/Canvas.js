3372d7790973121c2a40cd518e882e5b1f31f038
alexgrist14 <agurec.fasasf@gmail.com>
2019-06-07 12:32:39 +0300

    feat: add state to canvas

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -4,41 +4,43 @@ import React, { Component } from 'react';
 export default class Canvas extends Component {
   constructor(props) {
     super(props);
-    this.canvas = document.querySelector('main-canvas');
+    this.state = {
+      canvas: document.querySelector('.main-canvas'),
+    };
+    this.drawCanvas = this.drawCanvas.bind(this);
   }
 
   // eslint-disable-next-line class-methods-use-this
   drawCanvas() {
-    const canvas = document.querySelector('.main-canvas');
-    const context = canvas.getContext('2d');
+    // const canvas = document.querySelector('.main-canvas');
+    const context = this.state.canvas.getContext('2d');
 
     const mouse = { x: 0, y: 0 };
     let draw = false;
-    canvas.addEventListener('mousedown', (e) => {
-      mouse.x = e.pageX - canvas.offsetLeft;
-      mouse.y = e.pageY - canvas.offsetTop;
+    this.state.canvas.addEventListener('mousedown', function (e) {
+      mouse.x = e.pageX - this.state.canvas.offsetLeft;
+      mouse.y = e.pageY - this.state.canvas.offsetTop;
       draw = true;
       context.beginPath();
       context.moveTo(mouse.x, mouse.y);
     });
-    canvas.addEventListener('mousemove', (e) => {
+    this.state.canvas.addEventListener('mousemove', function (e) {
       if (draw === true) {
-        mouse.x = e.pageX - canvas.offsetLeft;
-        mouse.y = e.pageY - canvas.offsetTop;
+        mouse.x = e.pageX - this.state.canvas.offsetLeft;
+        mouse.y = e.pageY - this.state.canvas.offsetTop;
         context.lineTo(mouse.x, mouse.y);
         context.stroke();
       }
     });
-    canvas.addEventListener('mouseup', (e) => {
-      mouse.x = e.pageX - canvas.offsetLeft;
-      mouse.y = e.pageY - canvas.offsetTop;
+    this.state.canvas.addEventListener('mouseup', function (e) {
+      mouse.x = e.pageX - this.state.canvas.offsetLeft;
+      mouse.y = e.pageY - this.state.canvas.offsetTop;
       context.lineTo(mouse.x, mouse.y);
       context.stroke();
       context.closePath();
       draw = false;
     });
   }
-
   render() {
     return (
       <canvas className="main-canvas" onMouseEnter={this.drawCanvas}>
