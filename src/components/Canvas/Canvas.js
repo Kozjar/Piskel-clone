482ae6189d99dfc767a1330299d12a7c57275385
alexgrist14 <agurec.fasasf@gmail.com>
2019-06-08 16:12:30 +0300

    feat: add partial pixel drawing

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -12,6 +12,7 @@ export default class Canvas extends Component {
     this.startDrawing = this.startDrawing.bind(this);
     this.mouseMove = this.mouseMove.bind(this);
     this.endDrawing = this.endDrawing.bind(this);
+    this.a = this.a.bind(this);
   }
 
   componentDidMount() {
@@ -21,38 +22,46 @@ export default class Canvas extends Component {
   startDrawing(e) {
     e.persist();
     this.context = this.canvas.getContext('2d');
-    this.mouse.x = e.pageX - this.canvas.offsetLeft;
-    this.mouse.y = e.pageY - this.canvas.offsetTop;
+    this.mouse.x = e.pageX - this.canvas.offsetLeft - 5;
+    this.mouse.y = e.pageY - this.canvas.offsetTop - 5;
     this.draw = true;
 
     this.context.beginPath();
-    this.context.moveTo(this.mouse.x, this.mouse.y);
+    // this.context.moveTo(this.mouse.x, this.mouse.y);
+    this.a();
   }
 
   mouseMove(e) {
     e.persist();
     this.context = this.canvas.getContext('2d');
     if (this.draw === true) {
-      this.mouse.x = e.pageX - this.canvas.offsetLeft;
-      this.mouse.y = e.pageY - this.canvas.offsetTop;
-
-      this.context.lineTo(this.mouse.x, this.mouse.y);
-      this.context.stroke();
+      this.mouse.x = e.pageX - this.canvas.offsetLeft - 5;
+      this.mouse.y = e.pageY - this.canvas.offsetTop - 5;
+      this.a();
     }
   }
 
   endDrawing(e) {
     e.persist();
     this.context = this.canvas.getContext('2d');
-    this.mouse.x = e.pageX - this.canvas.offsetLeft;
-    this.mouse.y = e.pageY - this.canvas.offsetTop;
+    this.mouse.x = e.pageX - this.canvas.offsetLeft - 5;
+    this.mouse.y = e.pageY - this.canvas.offsetTop - 5;
     this.draw = false;
-
-    this.context.lineTo(this.mouse.x, this.mouse.y);
-    this.context.stroke();
     this.context.closePath();
-
     this.props.onUpdateFramePreview(); // update active frame preview
+    this.a();
+  }
+
+  a() {
+    const imgData = this.context.createImageData(5, 5);
+    // const index = 4 * (this.mouse.x + this.mouse.y * 500);
+    for (let i = 0; i < imgData.data.length; i += 4) {
+      imgData.data[i + 0] = 255;
+      imgData.data[i + 1] = 0;
+      imgData.data[i + 2] = 0;
+      imgData.data[i + 3] = 255;
+    }
+    this.context.putImageData(imgData, this.mouse.x + 5, this.mouse.y + 5, 0, 0, 5, 5);
   }
 
   render() {
