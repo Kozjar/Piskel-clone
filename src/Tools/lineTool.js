afe1225aba9236db5c70431098e03d8c2c746c52
Kozjar <qwertzxc0000@gmail.com>
2019-06-27 12:48:53 +0300

    feat: add additional drawing canvas

--- src/Tools/lineTool.js
+++ src/Tools/lineTool.js
@@ -1,30 +1,27 @@
-import { drawLine, drawPixel } from '../managers/drawingManager';
+import { drawLine } from '../managers/drawingManager';
 
-let startPos = {
+const startPos = {
   x: 0,
   y: 0,
 };
 
 function mouseDown(e) {
-  this.context = this.canvas.getContext('2d');
   startPos.x = this.state.mouse.x;
   startPos.y = this.state.mouse.y;
   this.draw = true;
-  drawPixel.bind(this, this.state.mouse.x, this.state.mouse.y)();
 }
 
 function mouseMove(e) {
   if (this.draw === false) return;
-  this.context = this.canvas.getContext('2d');
-  const canvas = document.getElementById('main-canvas');
-  const context = canvas.getContext('2d');
 
-  context.clearRect(0, 0, canvas.width, canvas.height); //  clear cnavas
+  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); //  clear cnavas
   drawLine.bind(this)(startPos.x, startPos.y, this.state.mouse.x, this.state.mouse.y);
 }
 
 function mouseUp(e) {
   this.draw = false;
+  this.context = this.canvas.getContext('2d');
+  drawLine.bind(this)(startPos.x, startPos.y, this.state.mouse.x, this.state.mouse.y);
 }
 
 export {
--- src/Tools/penTool.js
+++ src/Tools/penTool.js
@@ -1,4 +1,3 @@
-import hlPixel from '../managers/highlightingManager';
 import { drawLine, drawPixel } from '../managers/drawingManager';
 
 function mouseDown(e) {
@@ -13,14 +12,13 @@ function mouseDown(e) {
 function mouseMove(e) {
   e.persist();
   if (this.draw === true) {
+    this.context = this.canvas.getContext('2d');
     if (this.state.mouse.x > this.state.mousePrev.x + 1
       || this.state.mouse.x < this.state.mousePrev.x - 1
       || this.state.mouse.y > this.state.mousePrev.y + 1
       || this.state.mouse.y < this.state.mousePrev.y - 1) {
-      console.log('drawLine');
       drawLine.bind(this)(this.state.mousePrev.x, this.state.mousePrev.y, this.state.mouse.x, this.state.mouse.y);
     } else {
-      console.log('drawPixel');
       drawPixel.bind(this)(this.state.mouse.x, this.state.mouse.y);
     }
   }
@@ -28,6 +26,7 @@ function mouseMove(e) {
 
 function mouseUp(e) {
   this.draw = false;
+  this.context = this.canvas.getContext('2d');
   drawPixel.bind(this)(this.state.mouse.x, this.state.mouse.y);
 }
 
