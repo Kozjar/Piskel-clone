ab21a2ab6276b491ac74bc6a9393570920f39dd0
Kozjar <qwertzxc0000@gmail.com>
2019-07-02 15:09:40 +0300

    fix: implement correct bucket tool

--- src/Tools/bucketTool.js
+++ src/Tools/bucketTool.js
@@ -1,27 +1,36 @@
+function compareDataColors(data, point, color) {
+  if (data[point + 0] === color.r
+    && data[point + 1] === color.g
+    && data[point + 2] === color.b) return true;
+  return false;
+}
+
 function mouseDown(e) {
   this.context = this.canvas.getContext('2d');
-  const imageData = this.context.getImageData(0, 0, 636, 636);
+  const imageData = this.context.getImageData(0, 0, this.state.canvasSize, this.state.canvasSize);
   const { width } = imageData;
   const { height } = imageData;
-  const color = 255;
-  const borderColor = 255;
-  const stack = [[Math.floor((e.pageX - this.canvas.offsetLeft) / this.state.scale), Math.floor((e.pageY - this.canvas.offsetTop) / this.state.scale)]];
+  const pixelData = this.canvas.getContext('2d').getImageData(this.state.mouse.x, this.state.mouse.y, 1, 1).data;
+  const color = { r: pixelData[0], g: pixelData[1], b: pixelData[2] };
+  const stack = [[this.state.mouse.x, this.state.mouse.y]];
   let pixel;
   let point = 0;
+  if (compareDataColors(pixelData, 0, this.props.mainColor)) return;
   while (stack.length > 0) {
     pixel = stack.pop();
-    if (pixel[0] < 0 || pixel[0] >= width)
-      continue;
-    if (pixel[1] < 0 || pixel[1] >= height)
-      continue;
+    if (pixel[0] < 0 || pixel[0] >= width) continue;
+    if (pixel[1] < 0 || pixel[1] >= height) continue;
 
     // Alpha
-    point = pixel[1] * 4 * width + pixel[0] * 4 + 3;
+    point = pixel[1] * 4 * width + pixel[0] * 4;
 
     // Если это не рамка и ещё не закрасили
-    if (imageData.data[point] !== borderColor && imageData.data[point] !== color) {
+    if (compareDataColors(imageData.data, point, color)) {
       // Закрашиваем
-      imageData.data[point] = color;
+      imageData.data[point] = this.props.mainColor.r;
+      imageData.data[point + 1] = this.props.mainColor.g;
+      imageData.data[point + 2] = this.props.mainColor.b;
+      imageData.data[point + 3] = 255;
 
       // Ставим соседей в стек на проверку
       stack.push([
@@ -36,11 +45,11 @@ function mouseDown(e) {
 
       stack.push([
         pixel[0],
-        pixel[1] - 1,
-        stack.push([
-          pixel[0],
-          pixel[1] + 1,
-        ]),
+        pixel[1] - 1]);
+
+      stack.push([
+        pixel[0],
+        pixel[1] + 1,
       ]);
     }
   }
