3f656fed1336bacd294726a810f274e5196b7e4f
Kozjar <qwertzxc0000@gmail.com>
2019-07-02 14:23:50 +0300

    feat: add right side tools layout

--- src/components/RightSideTools/Layers.js
+++ src/components/RightSideTools/Layers.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Layers extends Component {
   render() {
     return (
-      <div>
-
+      <div className='layers-container'>
+        Layers
       </div>
     );
   }
