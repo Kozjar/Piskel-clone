22979c9ce689a506c75ff0dbeeeffaf1ee3e6a6b
Kozjar <qwertzxc0000@gmail.com>
2019-07-13 16:13:42 +0300

    fix: fix bucket tool

--- src/Tools/bucketTool.js
+++ src/Tools/bucketTool.js
@@ -1,30 +1,28 @@
-
 function compareDataColors(data, point, color) {
   if (data[point + 0] === color.r
     && data[point + 1] === color.g
-    && data[point + 2] === color.b
-    && data[point + 3] === color.a) return true;
+    && data[point + 2] === color.b) return true;
   return false;
 }
 
-function mouseDown() {
+function mouseDown(e) {
   this.context = this.canvas.getContext('2d');
   const imageData = this.context.getImageData(0, 0, this.props.canvasSize, this.props.canvasSize);
-  const pixelData = this.context.getImageData(this.state.mouse.x, this.state.mouse.y, 1, 1).data;
-  const color = {
-    r: pixelData[0], g: pixelData[1], b: pixelData[2], a: pixelData[3],
-  };
+  const { width } = imageData;
+  const { height } = imageData;
+  const pixelData = this.canvas.getContext('2d').getImageData(this.state.mouse.x, this.state.mouse.y, 1, 1).data;
+  const color = { r: pixelData[0], g: pixelData[1], b: pixelData[2] };
   const stack = [[this.state.mouse.x, this.state.mouse.y]];
   let pixel;
   let point = 0;
   if (compareDataColors(pixelData, 0, this.props.mainColor)) return;
   while (stack.length > 0) {
     pixel = stack.pop();
-    if (pixel[0] < 0 || pixel[0] >= this.props.canvasSize) continue;
-    if (pixel[1] < 0 || pixel[1] >= this.props.canvasSize) continue;
+    if (pixel[0] < 0 || pixel[0] >= width) continue;
+    if (pixel[1] < 0 || pixel[1] >= height) continue;
 
     // Alpha
-    point = pixel[1] * 4 * this.props.canvasSize + pixel[0] * 4;
+    point = pixel[1] * 4 * width + pixel[0] * 4;
 
     // Если это не рамка и ещё не закрасили
     if (compareDataColors(imageData.data, point, color)) {
@@ -47,8 +45,7 @@ function mouseDown() {
 
       stack.push([
         pixel[0],
-        pixel[1] - 1,
-      ]);
+        pixel[1] - 1]);
 
       stack.push([
         pixel[0],
@@ -63,4 +60,3 @@ function mouseMove() { }
 function mouseUp() { }
 
 export { mouseDown, mouseMove, mouseUp };
-
