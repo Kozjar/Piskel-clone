a432cef0cd643a651c9e6ed210800515f266014d
alexgrist14 <agurec.fasasf@gmail.com>
2019-07-16 12:30:58 +0300

    feat: add working animation-prewiev

--- src/components/RightSideTools/AnimPreview.js
+++ src/components/RightSideTools/AnimPreview.js
@@ -7,48 +7,47 @@ export default class AnimPreview extends Component {
   constructor(props) {
     super(props);
     this.frames = undefined;
-    this.animation = this.animation.bind(this);
     this.state = {
       counter: 0,
-      FPS: 1,
+      FPS: 12,
     };
-    this.interval = null;
+    this.interval = undefined;
+
+    this.changeFPSCounter = this.changeFPSCounter.bind(this);
+    this.startAnimation = this.startAnimation.bind(this);
   }
 
-  componentDidMount() {
-    this.interval = (delay) => {
-      setInterval(() => {
-        this.frames = document.querySelectorAll('.frames-bar__frame-preview-canvas');
-        const images = document.getElementById('animation-preview');
-        // const ctx = this.frames[0].getContext('2d');
-        // const img = ctx.getImageData(0, 0, 32, 32);
-        images.src = this.frames[this.state.counter].toDataURL();
-        this.setState({
-          counter: this.state.counter += 1,
-        });
+  startAnimation(delay) {
+    this.interval = setInterval(() => {
+      this.frames = document.querySelectorAll('.frames-bar__frame-preview-canvas');
+      const images = document.getElementById('animation-preview');
+      images.src = this.frames[this.state.counter].toDataURL();
+      this.setState({
+        counter: this.state.counter += 1,
+      });
 
-        if (this.state.counter === this.frames.length) {
-          this.setState({
-            counter: this.state.counter = 0,
-          });
-        }
-      }, delay);
-    };
+      if (this.state.counter === this.frames.length) {
+        this.setState({
+          counter: this.state.counter = 0,
+        });
+      }
+    }, delay);
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.FPS !== prevState.FPS) {
-      console.log('update');
-      this.interval(1000 / this.state.FPS);
+    
+        clearInterval(this.interval);
+        this.startAnimation(1000 / this.state.FPS);
+      
     }
   }
 
-  animation() {
+  changeFPSCounter() {
     const range = document.querySelector('.frame-rate');
     this.setState({
       FPS: range.value,
     });
-    global.console.log(this.state.FPS);
   }
 
   render() {
@@ -57,7 +56,7 @@ export default class AnimPreview extends Component {
         <div className='animation-preview'><img id='animation-preview' src="" alt="" /></div>
         <div className='fps-slider'>
           <div className='fps-counter'>{this.state.FPS} FPS</div>
-          <input className='frame-rate' type="range" min="0" max="24" step="1" defaultValue="0" onChange={this.animation} />
+          <input className='frame-rate' type="range" min="1" max="24" step="1" defaultValue="12" onChange={this.changeFPSCounter} />
         </div>
       </div>
     );
